529b9e9e2e354e64342e7f80f1383248d1bd12e3
Ben Horn <benh@milan01.iwr.uni-heidelberg.de>
2024-08-12 09:57:10 +0200

    dune-evaluate-solvers.cc now working

---
+++ dependencies/DUNE/buildmodules.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ROOT=$(pwd)
+BUILDDIR=$ROOT/release-build
+OPTSFILE=release.opts
+MODULES="common istl evaluation "
+for i in $MODULES; do
+    echo build $i
+    ./dune-common/bin/dunecontrol --builddir=$BUILDDIR  --opts=$OPTSFILE --only=dune-$i all
+done
+
