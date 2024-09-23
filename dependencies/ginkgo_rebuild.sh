e091cefa4b959e5f37aee1aea475b9ea47628384
Ben Horn <benh@milan01.iwr.uni-heidelberg.de>
2024-09-23 12:31:22 +0200

    added ginkgo_rebuild skript

--- dependencies/ginkgo_rebuild.sh
+++ dependencies/ginkgo_rebuild.sh
@@ -6,16 +6,9 @@ ginkgo_download_name="v$ginkgo_version.tar.gz"
 set -x
 set -e
 
-# install
-mkdir GINKGO
+# rebuild
 cd GINKGO
-# download
-wget https://github.com/ginkgo-project/ginkgo/archive/refs/tags/v1.8.0.tar.gz
-tar -xvf $ginkgo_download_name
-rm $ginkgo_download_name
-# build
 cd ginkgo-$ginkgo_version
-mkdir build
 cd build
 #cmake -DGINKGO_CUDA_ARCHITECTURES=Turing\;75 ..
 cmake -DGINKGO_BUILD_CUDA=OFF ..
