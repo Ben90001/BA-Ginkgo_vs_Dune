d414129365e83485e98e43ebe8b3690facffb498
Ben Horn <benh@milan01.iwr.uni-heidelberg.de>
2024-09-23 17:41:24 +0200

    improved and adjusted generate-results.sh skript to recent changes

--- src/generate-results.sh
+++ src/generate-results.sh
@@ -4,12 +4,22 @@
 set -x
 set -e
 
-n_max="200"
-rounds="5"
+n_lowerBound="1"
+n_upperBound="400"
+rounds="4"
+interval="1"
+device="cpu"
+executors="ref omp"
+mtx_formats="csr coo ell"
 
 rm -rf results
 mkdir results
 cd results
-./../../dependencies/DUNE/release-build/dune-evaluation/src/dune-evaluation $n_max $rounds
-./../ginkgo/build/ginkgo-evaluation $n_max $rounds
 
+./../../dependencies/DUNE/release-build/dune-evaluation/src/dune-evaluation $n_max $rounds
+
+for executor in "${executors[@]}"; do
+    for mtx_format in "${mtx_formats[@]}"; do
+    ./../ginkgo/build/ginkgo-evaluation $n_lowerBound $n_upperBound $rounds $interval $device $executor $mtx_format
+    done
+done
