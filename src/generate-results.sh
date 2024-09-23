af258b4188d2f0a8a9237a4213d1eb64c0358a3d
Ben Horn <benh@milan01.iwr.uni-heidelberg.de>
2024-09-23 17:45:53 +0200

    added timing to generation skript

--- src/generate-results.sh
+++ src/generate-results.sh
@@ -15,11 +15,17 @@ mtx_formats="csr coo ell"
 rm -rf results
 mkdir results
 cd results
+# Create a log file
+log_file="execution_log.txt"
+echo "Script execution started at: $(date)" > $log_file
 
+# generate data -------------------------------------------------------------------------------------------------------
 ./../../dependencies/DUNE/release-build/dune-evaluation/src/dune-evaluation $n_max $rounds
-
 for executor in "${executors[@]}"; do
     for mtx_format in "${mtx_formats[@]}"; do
     ./../ginkgo/build/ginkgo-evaluation $n_lowerBound $n_upperBound $rounds $interval $device $executor $mtx_format
     done
 done
+# ---------------------------------------------------------------------------------------------------------------------
+
+echo "Script execution ended at: $(date)" >> $log_file
