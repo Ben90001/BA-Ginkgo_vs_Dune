b9a9c1abc5c649a91340d83836b14709c9144db2
Ben Horn <benh@milan01.iwr.uni-heidelberg.de>
2025-02-03 11:30:34 +0100

    added type size output

--- src/sys_info/sys_info.cc
+++ src/sys_info/sys_info.cc
@@ -4,6 +4,8 @@
 
 int main() {
     std::cout << "size_t is " << sizeof(size_t) * 8 << " bits.\n";
+    std::cout << "long is " << sizeof(long) * 8 << " bits.\n";
+    std::cout << "long long is " << sizeof(long long) * 8 << " bits.\n";
     std::cout << "int is " << sizeof(int) * 8 << " bits.\n";
 
     if constexpr (std::is_same_v<std::chrono::high_resolution_clock, std::chrono::steady_clock>) {
